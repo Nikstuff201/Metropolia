fe4d263f98952fc565e4260754531babc6f17714
Nikita Rybakov <rubakov.n92@gmail.com>
2025-01-20 17:42:41 +0200

    Tehtävä 3 done

--- Tehtävä 2/3.py
+++ Tehtävä 2/3.py
@@ -1,2 +1,6 @@
 kanta=float(input("Anna kannan pituus: "))
 korkeus=float(input("Anna korkeuden pituus: "))
+pintaala=kanta*korkeus
+piiri=kanta*2+korkeus*2
+print(f"Pinta-ala={pintaala}")
+print(f"Piiri={piiri}")
