c1189cf582b0bcd48a392e9a374b10dba0865048
Nikita Rybakov <rubakov.n92@gmail.com>
2025-01-22 09:52:57 +0200

    Tehtävä 6 done

---
+++ Moduuli 2/Tehtävä 6.py
@@ -0,0 +1,5 @@
+import random
+koodi1=(random.randint(0,9),random.randint(0,9),random.randint(0,9))
+print(f"Koodi 1: {koodi1[0]}{koodi1[1]}{koodi1[2]}")
+koodi2=(random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6))
+print(f"koodi 2: {koodi2[0]}{koodi2[1]}{koodi2[2]}{koodi2[3]}")
