58054ea92be3c68ab9cfe225cdf73341103f1a0d
Nikita Rybakov <rubakov.n92@gmail.com>
2025-01-22 09:33:50 +0200

    Tehtävä 5 done

--- Moduuli 2/Tehtävä 5.py
+++ Moduuli 2/Tehtävä 5.py
@@ -1,6 +1,6 @@
 leiviskät=float(input("Anna leiviskät:\n"))
-naulat=float(input("Anna naulat:\n"))
-luodit=float(input("Anna luodit:\n"))
+naulat=float(input("\nAnna naulat:\n"))
+luodit=float(input("\nAnna luodit:\n"))
 summa=leiviskät*20*32*13.3+naulat*32*13.3+luodit*13.3
-print("Massa nykymittojen mukaan:")
-print(f"{summa//1000} kilogrammaa ja {summa%1000:.2f} gramma")
+print("\nMassa nykymittojen mukaan:")
+print(f"{int(summa//1000)} kilogrammaa ja {summa%1000:.2f} gramma")
