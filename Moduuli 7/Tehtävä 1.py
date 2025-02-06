733d2118ad81bb88b46101b45ea0fe0f119f6cba
Nikita Rybakov <rubakov.n92@gmail.com>
2025-02-06 19:38:53 +0200

    Moduuli 7 Tehtävä 1,2 done

---
+++ Moduuli 7/Tehtävä 1.py
@@ -0,0 +1,17 @@
+vuodenajat={
+    12: "talvi",
+    1: "talvi",
+    2: "talvi",
+    3: "kevät",
+    4: "kevät",
+    5: "kevät",
+    6: "kesä",
+    7: "kesä",
+    8: "kesä",
+    9: "syksy",
+    10: "syksy",
+    11: "syksy",
+}
+
+kuukausi=int(input("Anna kuukauden numero: "))
+print (vuodenajat[kuukausi])
