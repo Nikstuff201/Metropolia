fd945f192bad19ec55c88cda4849bfd1f760b64d
Nikita Rybakov <rubakov.n92@gmail.com>
2025-03-26 18:27:04 +0200

    Moduuli 12 tehtävä 2 done

--- Moduuli 12/Tehtävä 2.py
+++ Moduuli 12/Tehtävä 2.py
@@ -1,8 +1,8 @@
 import requests
 import json
-
+apikey=input("Anna apikey: ")
 paikkakunta=input("Anna paikkakunnan nimi: ")
-pyyntö=f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta},&APPID=0dfcd980a5b0bc8c920c471163b44469"
+pyyntö=f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta},&APPID={apikey}"
 
 vastaus=requests.get(pyyntö).json()
 print(vastaus["weather"][0]["description"]+f", {vastaus["main"]["temp"]-273.15:.2f} ℃")
